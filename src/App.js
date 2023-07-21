cb36dd0f2438fc298fe2de866fbd8e005a3177a7
AnoopAG91 <anoopag47@gmail.com>
2023-07-21 23:36:49 +0530

    created new todo app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 
 import './App.css';
+import Todo from './Todo/todo';
 
 function App() {
   return (
     <div className="App">
-      
+      <Todo/>
     </div>
   );
 }
---
+++ src/Todo/todo.js
@@ -0,0 +1,50 @@
+import React, { useState } from 'react'
+import './todo.css'
+
+function Todo() {
+    const [addItems, setaddItems] = useState("") // This usestate is for storing and updating the input datas.
+    
+    const[addList,setaddList] = useState([])  // This usestate is for adding all the input datas to an array when the button get clicked.
+    
+    function handlebtn (){  // handle button is an onclick function where all te datas get stored to the array
+        setaddList([...addList,addItems])  // setaddlist updates the state of addlist variable. 
+                                             // spread operator used to creates a new array of addlist and 
+                                                // it will add the updated values from the additems variable.
+        setaddItems ("")  // this function will clear the input field after the button is clicked.
+       console.log(addList)
+    }
+
+    function removeItems(i){  // this function is for removing the added list datas. it is taking i(index) as a parameter.
+        const updatedList = addList.filter((elem, id)=>{   // using filter method to filter out the list data. 
+            return i!==id  // retrn if index not equalto id.
+        }) 
+        setaddList(updatedList)   // updating the addlist variable with the updated list..
+    }
+
+  return (
+    <div className='todo-wrapper'>
+        <h1>TODO APP</h1>
+        <input type='text' placeholder='addItems' value={addItems} onChange={(e)=>setaddItems(e.target.value)}/>
+        <button className='add-btn' onClick={handlebtn}>ADD</button>
+
+       
+        {
+            addList!==[] && addList.map((data, i) =>{   // map method used to go through each elements in the array with its index. data represents each elements in the array.
+
+                return (      // returning each of the elements in the array 
+                      
+                    <>
+                    
+                     <div><h4>{data}</h4>   <button className='remove-btn' onClick={()=>removeItems(i)}>Remove</button></div>{/* data represents each elements ib= the array and prints it in an h4 tag */}
+                     {/*  delete button takes onclick event function as
+                                                                               arrow function with (i) as parameter */}
+                    </>
+                
+                )
+            })
+        }
+    </div>
+  )
+}
+
+export default Todo
